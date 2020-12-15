f3cc0a871e689775e7d50671354214893469a7a9
suro-sk <surokos93>
2020-12-15 17:07:28 +0400

    Function Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+    return (
+        <h1>Hello <User name={"Elon Mask"}/>!</h1>
+    )
+}
+
+function Footer() {
+    return (
+        <h4>Have a good day</h4>
+    )
+}
+
+function User(props) {
+    return (
+        <span>{props.name || 'John Doe'}</span>
+    )
+}
+
+function CurrentDay() {
+    let currentDate = new Date();
+    return (
+        <div>
+            <h3>Today is:</h3>
+            <h2>{currentDate.toDateString()}</h2>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Header/>
+                <CurrentDay/>
+                <Footer/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
