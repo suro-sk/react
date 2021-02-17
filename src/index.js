81c2543b6a602e572751a12fabde067bab2ede99
suro-sk <surokos93>
2021-02-17 20:18:52 +0400

    Homework-27

--- src/index.js
+++ src/index.js
@@ -10,16 +10,18 @@ function reducer(state = {count: 0}, action) {
     switch (action.type) {
         case 'INCREMENT_COUNT':
             return {
+                ...state,
                 count: state.count + 1
             }
         case 'DECREMENT_COUNT':
             return {
+                ...state,
                 count: state.count - 1
             }
         default:
             return state;
     }
-    
+
 }
 
 const store = createStore(reducer);
