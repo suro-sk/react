59925c651a31495183889597b5ab362acb9bb64c
suro-sk <surokos93>
2021-03-29 09:19:00 +0400

    Final Changes

--- src/helpers/authToken.js
+++ src/helpers/authToken.js
@@ -57,7 +57,7 @@ export function getToken() {
                     return token.jwt;
                 })
                 .catch(() => {
-                    // logOut();
+                    logOut();
                 });
         }
     }
